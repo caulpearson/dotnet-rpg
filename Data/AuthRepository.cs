c7863dba554465ed661e17da3a7b82d19619d9a7
Caulfield Pearson <caulpearson@gmail.com>
2022-08-14 23:20:08 -0500

    authentication repository

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public Task<ServiceResponse<string>> Login(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
