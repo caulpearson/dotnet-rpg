d9ddc6f200ca1fd1ba10cc716480121441635f14
Caulfield Pearson <caulpearson@gmail.com>
2022-08-18 23:59:03 -0500

    Added AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using dotnet_rpg.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+    }
+}
